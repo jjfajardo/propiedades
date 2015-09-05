3ac8a869f0e51a0b1f8a4b19f249562665d79f35
Alex Dzul <alexexc2@gmail.com>
2015-09-05 01:39:45 -0500

    allow cors.

--- resty/settings.py
+++ resty/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'resty.apps.properties',
     'rest_framework',
+    'corsheaders',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -110,3 +112,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.AllowAny',),
     'PAGE_SIZE': 10
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
