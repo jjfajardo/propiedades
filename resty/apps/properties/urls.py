8b16dc54c39bf2e4ad4a57bcf4a2bef23dbbc9cd
Alex Dzul <alexexc2@gmail.com>
2015-09-04 23:52:35 -0500

    Se envía la primera prueba para validar si un código no está tomado.

--- resty/apps/properties/urls.py
+++ resty/apps/properties/urls.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from .views import HouseList, AddHouse
+from .webservices import urls as url_ws
 
 urlpatterns = patterns('',
                        url(r'^$', HouseList.as_view(), name="list"),
                        url(r'^add/$', AddHouse.as_view(), name="add"),
+                       url(r'ws/', include(url_ws, namespace='ws')),
                        )
+
