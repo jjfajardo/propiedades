831cd8d71f35a57499789f911e8ef87677c53fe6
Alex Dzul <alexexc2@gmail.com>
2015-09-05 01:18:05 -0500

    limpieza de ws del master demo.

--- resty/apps/properties/urls.py
+++ resty/apps/properties/urls.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url, include
+from django.conf.urls import patterns, url
 from .views import HouseList, AddHouse
-from .webservices import urls as url_ws
 
 urlpatterns = patterns('',
                        url(r'^$', HouseList.as_view(), name="list"),
                        url(r'^add/$', AddHouse.as_view(), name="add"),
-                       url(r'ws/', include(url_ws, namespace='ws')),
                        )
 
--- resty/apps/properties/webservices/urls.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
-from .views import ValidateCode
-
-urlpatterns = patterns('',
-                       url(r'^validate/code/$', ValidateCode.as_view(), name="code"),
-                       )
